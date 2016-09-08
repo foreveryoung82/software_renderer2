c072b3efdb5668a07f80dcdeb3f9bc8c1c4b259e
jieyang <51790592@qq.com>
2016-09-08 15:44:59 +0800

    - clean code:remove unnecessary header files

--- software_renderer2/include/deviceparameters.h
+++
@@ -1,9 +0,0 @@
-//@date:2016.9.1
-//@author:yangjie82@gmail.com
-//@brief:parameters to create device
-#pragma once
-
-struct DeviceParameters {
-  int framebufferWidth;
-  int framebufferHeight;
-};
